6b2c81792855307ff4dbce92d2c441da13aaf4bd
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2024-10-07 22:48:12 +0530

    Add files via upload

---
+++ Task2/StudentGradeCalculator.java
@@ -0,0 +1,46 @@
+package Task2;
+
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter the number of subjects : ");
+		int numSub = scan.nextInt();
+		int []marks =new int[numSub];
+		int totalmarks=0;
+		
+		for(int i=1;i<=numSub;i++) {
+			System.out.println("Enter marks of subject "+i+" (out of 100) : ");
+			marks[i-1]=scan.nextInt();
+			totalmarks+=marks[i-1];
+		}
+		double  avgPercentage =(double)totalmarks/numSub;
+		
+		String grade = findGrade(avgPercentage);
+		
+		System.out.println("...........Result............ ");
+		System.out.println("Total Marks : "+totalmarks);
+		System.out.println("Average Percentage : "+avgPercentage);
+		System.out.println("Grade : "+grade);
+	}
+
+	private static String findGrade(double avgPercentage) {
+		if(avgPercentage>=90) {
+			return "A+";
+		}else if(avgPercentage>=80) {
+			return "A";
+		}else if (avgPercentage>=70) {
+			return "B+";
+		}else if (avgPercentage>=60) {
+			return "B";
+		}else if (avgPercentage>=50) {
+			return "C+";
+		}else if (avgPercentage>=40) {
+			return "D";
+		} else {
+				return "F";
+		}
+	}
+}
