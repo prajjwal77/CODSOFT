70858ad57aef4ca2ebc06a211d22bd50966e5b43
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2024-10-16 22:57:01 +0530

    Add files via upload

---
+++ Student-Management-System/src/main/Model/Student.java
@@ -0,0 +1,55 @@
+package com.Student.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+		@Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+
+	    private String name;
+	    
+	    private int rollNumber;
+	    
+	    private String grade;
+	    
+	    private int age;
+	    
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getRollNumber() {
+			return rollNumber;
+		}
+		public void setRollNumber(int rollNumber) {
+			this.rollNumber = rollNumber;
+		}
+		public String getGrade() {
+			return grade;
+		}
+		public void setGrade(String grade) {
+			this.grade = grade;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+	    
+	    
+}
